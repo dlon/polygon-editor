7612cdb0a699b51115ef09f2738f110660d5f0df
AurelienRibon <none@none>
2012-06-27 08:48:16 +0200

    ! fixed previous cleanup

--- src/aurelienribon/libgdx/polygoneditor/Assets.java
+++ src/aurelienribon/libgdx/polygoneditor/Assets.java
@@ -2,7 +2,6 @@ package aurelienribon.libgdx.polygoneditor;
 
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 /**
  * @author Aurelien Ribon | http://www.aurelienribon.com/
@@ -15,13 +14,11 @@ public class Assets extends AssetManager {
 		manager.load("res/data/white.png", Texture.class);
 		manager.load("res/data/transparent-light.png", Texture.class);
 		manager.load("res/data/transparent-dark.png", Texture.class);
-		manager.load("res/data/particles.pack", TextureAtlas.class);
 		manager.finishLoading();
 	}
 
 	public static Texture getTransparentLightTex() {return manager.get("res/data/transparent-light.png", Texture.class);}
 	public static Texture getTransparentDarkTex() {return manager.get("res/data/transparent-dark.png", Texture.class);}
 	public static Texture getWhiteTex() {return manager.get("res/data/white.png", Texture.class);}
-	public static TextureAtlas getParticlesAtlas() {return manager.get("res/data/particles.pack", TextureAtlas.class);}
 	public static Texture getIcon(String path) {return manager.get(path, Texture.class);}
 }
